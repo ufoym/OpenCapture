01a12caad53abc10861b047289ea2255f6be1256
Ming <a@ufoym.com>
2016-11-30 14:05:34 +0800

    Add working example

--- src/main.cpp
+++ src/main.cpp
@@ -1,72 +1,37 @@
-#include <new>
-#include <windows.h>
-#include <windowsx.h>
-#include <mfapi.h>
-#include <mfidl.h>
-#include <mferror.h>
-#include <mfcaptureengine.h>
-#include <shlwapi.h>
-#include <strsafe.h>
-#include <commctrl.h>
-#include <d3d11.h>
-#include <iostream>
-
-
-struct DeviceParam
-{
-	DeviceParam() : pAttributes(NULL), ppDevices(NULL), count(0) {}
-	~DeviceParam()
-	{
-		for (DWORD i = 0; i < count; i++)
-			release(&ppDevices[i]);
-		CoTaskMemFree(ppDevices);
-		release(&pAttributes);
-	}
-	template <class T> inline void release(T **ppT)
-	{
-		if (*ppT) {
-			(*ppT)->Release();
-			*ppT = NULL;
-		}
-	}
-	IMFAttributes *pAttributes;
-	IMFActivate **ppDevices;
-	UINT32      count;
-	UINT32      selection;
-};
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include "CameraDS.h"
 
 int main()
 {
-	DeviceParam param;
-
-	HRESULT hr = MFCreateAttributes(&param.pAttributes, 1);
-	if (FAILED(hr))
-		return -1;
-	hr = param.pAttributes->SetGUID(
-		MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE,
-		MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE_VIDCAP_GUID);
-	if (FAILED(hr))
-		return -1;
-
-	// Enumerate devices.
-	hr = MFEnumDeviceSources(param.pAttributes, &param.ppDevices, &param.count);
-	if (FAILED(hr))
-		return -1;
-
-	for (DWORD i = 0; i < param.count; i++)
-	{
-		WCHAR *szFriendlyName = NULL;
-		UINT32 cchName;
-
-		hr = param.ppDevices[i]->GetAllocatedString(
-			MF_DEVSOURCE_ATTRIBUTE_FRIENDLY_NAME,
-			&szFriendlyName, &cchName);
-		if (FAILED(hr))
-			break;
-
-		std::wcout << szFriendlyName << std::endl;
-		CoTaskMemFree(szFriendlyName);
+	int camCount = CCameraDS::CameraCount();
+	CCameraDS camDS;
+	for (int i = 0; i < camCount; i++) {
+		char szCamName[1024];
+		int retval = camDS.CameraName(i, szCamName, sizeof(szCamName));
+		if (retval > 0)
+			printf("Camera #%d: %s\n", i, szCamName);
+		else
+			printf("Camera #%d: no name\n", i);
 	}
 
+	const int camID = 2;
+	if (!camDS.OpenCamera(camID, true))
+		printf("Camera #%d cannot be opened.\n", camID);
+
+	for (;;) {
+		IplImage *pFrame = camDS.QueryFrame();
+		if (pFrame == NULL) {
+			printf("Cannot grab frames from Camera #%d.\n", camID);
+			break;
+		}
+		cv::Mat frame = cv::cvarrToMat(pFrame);
+		cv::resize(frame, frame, cv::Size(1280, 800), 0, 0, cv::INTER_CUBIC);
+		cv::imshow("input", frame);
+		if (cv::waitKey(33) == 27)
+			break;
+	}
+	cv::destroyAllWindows();
+	
 	return 0;
 }
